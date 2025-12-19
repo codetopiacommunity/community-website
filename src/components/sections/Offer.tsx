e65329f983d59dbaea011234ad7e26ea3dda1a8c
iamcerebrocerberus <sethamekmen@gmail.com>
2025-12-19 02:39:26 +0000

    feat: add 'what we offer' section

--- src/components/sections/Offer.tsx
+++ src/components/sections/Offer.tsx
@@ -4,40 +4,25 @@ import { spaceGrotesk } from "../../fonts/fonts";
 export default function Offer() {
   return (
     <section
-      className={`bg-black/90 ${spaceGrotesk.className} p-3')]`}
+      className={`relative bg-black/90 ${spaceGrotesk.className} p-3 `}
     >
-      <div className="absolute top-1/2 left-0 transform -translate-y-1/2">
-        <img
-          src="/images/codetopia-logo-mark.png"
-          alt="Backend Left"
-          className="h-40 w-40 md:h-72 md:w-72 lg:h-96 lg:w-96 object-contain"
-        />
-      </div>
+      {/* Left Background Image */}
+      <div className="absolute top-0 left-[-340px] h-full w-[100%] z-0 bg-[url('/images/codetopia-logo.png')] bg-left bg-contain bg-no-repeat pointer-events-none"></div>
 
-      {/* Right Image */}
-      <div className="absolute top-1/2 right-0 transform -translate-y-1/2">
-        <img
-          src="/images/codetopia-logo-mark.png"
-          alt="Backend Right"
-          className="h-40 w-40 md:h-72 md:w-72 lg:h-96 lg:w-96 object-contain"
-        />
-      </div>
-      <div className="lg:max-w-[80%] mx-auto p-8">
+      {/* Right Background Image */}
+      <div className="absolute top-0 right-[-410px] h-full w-[100%] z-0 bg-[url('/images/codetopia-logo.png')] bg-right bg-contain bg-no-repeat pointer-events-none"></div>
+
+      {/* Section Content */}
+      <div className="relative z-10 lg:max-w-[80%] mx-auto py-20">
         <h2 className="text-center mb-10 uppercase">
           <span className="inline-block text-2xl font-medium text-white border-b-2 pb-2">
             What We Offer
           </span>
         </h2>
-        <div className="flex flex-wrap gap-4">
+
+        <div className="flex flex-wrap gap-4 justify-center">
           {/* Card 1 */}
-          <div
-            className="
-          bg-white basis-[18rem] min-w-[14rem] flex-1 p-6
-          shadow-[8px_8px_0px_0px_rgba(255,255,255,0.4)]
-          transition-shadow duration-300 ease-out
-          hover:shadow-[12px_12px_0px_0px_rgba(255,255,255,0.4)]
-        "
-          >
+          <div className="bg-white basis-[18rem] min-w-[14rem] flex-1 p-6 shadow-[8px_8px_0_0_rgba(255,255,255,0.4)] transition-shadow duration-300 ease-out hover:shadow-[12px_12px_0_0_rgba(255,255,255,0.4)]">
             <Image
               src="/images/icon-events.png"
               alt="event"
@@ -51,14 +36,7 @@ export default function Offer() {
           </div>
 
           {/* Card 2 */}
-          <div
-            className="
-          bg-white basis-[18rem] min-w-[14rem] flex-1 p-6
-          shadow-[8px_8px_0px_0px_rgba(255,255,255,0.4)]
-          transition-shadow duration-300 ease-out
-          hover:shadow-[12px_12px_0px_0px_rgba(255,255,255,0.4)]
-        "
-          >
+          <div className="bg-white basis-[18rem] min-w-[14rem] flex-1 p-6 shadow-[8px_8px_0_0_rgba(255,255,255,0.4)] transition-shadow duration-300 ease-out hover:shadow-[12px_12px_0_0_rgba(255,255,255,0.4)]">
             <Image
               src="/images/icon-events.png"
               alt="event"
@@ -72,14 +50,7 @@ export default function Offer() {
           </div>
 
           {/* Card 3 */}
-          <div
-            className="
-          bg-white basis-[18rem] min-w-[14rem] flex-1 p-6
-          shadow-[8px_8px_0px_0px_rgba(255,255,255,0.4)]
-          transition-shadow duration-300 ease-out
-          hover:shadow-[12px_12px_0px_0px_rgba(255,255,255,0.4)]
-        "
-          >
+          <div className="bg-white basis-[18rem] min-w-[14rem] flex-1 p-6 shadow-[8px_8px_0_0_rgba(255,255,255,0.4)] transition-shadow duration-300 ease-out hover:shadow-[12px_12px_0_0_rgba(255,255,255,0.4)]">
             <Image
               src="/images/icon-events.png"
               alt="event"
