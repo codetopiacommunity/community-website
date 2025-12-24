e94d5ebbed6d5b1a9f3705d6b34f1aa4dbaa5a53
iamcerebrocerberus <sethamekmen@gmail.com>
2025-12-24 18:08:36 +0000

    add box shadow to 'our growing comm' cards

--- src/components/sections/GrowingCommunity.tsx
+++ src/components/sections/GrowingCommunity.tsx
@@ -17,7 +17,8 @@ export default function GrowingCommunity() {
 
         <div className="flex flex-col md:flex-row gap-4 h-auto md:h-[300px]">
           {/* Left card */}
-          <div className="relative flex flex-col justify-end p-8 flex-1 bg-[url('/images/stats-bg-1.png')] bg-cover bg-center">
+          <div className="relative flex flex-col justify-end p-8 flex-1 bg-[url('/images/stats-bg-1.png')] bg-cover bg-center shadow-[5px_5px_0_0_rgba(0,0,0,0.4)]
+                transition-shadow duration-300 hover:shadow-[3px_3px_0_0_rgba(0,0,0,0.5)]">
             {/* Diagonal overlay */}
             <div className="absolute inset-0 bg-gradient-to-tl from-black/100 to-transparent pointer-events-none"></div>
 
@@ -30,14 +31,16 @@ export default function GrowingCommunity() {
 
           {/* Right column (two stacked cards) */}
           <div className="flex flex-col gap-4 flex-1">
-            <div className="relative flex flex-col  justify-end p-8 flex-1 bg-[url('/images/stats-bg-2.png')] bg-cover bg-center">
+            <div className="relative flex flex-col  justify-end p-8 flex-1 bg-[url('/images/stats-bg-2.png')] bg-cover bg-center shadow-[5px_5px_0_0_rgba(0,0,0,0.4)]
+                transition-shadow duration-300 hover:shadow-[3px_3px_0_0_rgba(0,0,0,0.5)]">
                 <div className="absolute inset-0 bg-gradient-to-tl from-black/100 to-transparent pointer-events-none"></div>
               <p className="relative text-right text-white">
                 <span className="block text-4xl font-bold">13+</span>
                 <span className="block text-sm">Workshops &amp; meetups</span>
               </p>
             </div>
-            <div className="relative flex flex-col justify-end p-8 flex-1 bg-[url('/images/stats-bg-2.png')] bg-cover bg-center">
+            <div className="relative flex flex-col justify-end p-8 flex-1 bg-[url('/images/stats-bg-2.png')] bg-cover bg-center shadow-[5px_5px_0_0_rgba(0,0,0,0.4)]
+                transition-shadow duration-300 hover:shadow-[3px_3px_0_0_rgba(0,0,0,0.5)]">
                 <div className="absolute inset-0 bg-gradient-to-tl from-black/100 to-transparent pointer-events-none"></div>
               <p className="relative text-right text-white">
                 <span className="block text-4xl font-bold">7+</span>
