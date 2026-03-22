e994adf0ed56732f906e67e6cf310b6933c0656d
iamcerebrocerberus <sethamekmen@gmail.com>
2026-03-22 17:17:43 +0000

    fix: linting

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL 
-      ? process.env.DATABASE_URL 
+    url: process.env.DATABASE_URL
+      ? process.env.DATABASE_URL
       : "postgresql://dummy:dummy@localhost:5432/dummy",
   },
 });
