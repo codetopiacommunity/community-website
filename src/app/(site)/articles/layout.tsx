0dc13336ccad108a07cb0b72fa12f9b712eaeddc
tette <tettehsaintluke@gmail.com>
2026-04-07 12:13:00 +0000

    style: fix Biome import order and formatting

--- src/app/(site)/articles/layout.tsx
+++ src/app/(site)/articles/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { ContentThemeProvider } from "@/components/layout/ThemeProvider";
 import type { ReactNode } from "react";
+import { ContentThemeProvider } from "@/components/layout/ThemeProvider";
 
 export default function ArticlesLayout({ children }: { children: ReactNode }) {
   return <ContentThemeProvider>{children}</ContentThemeProvider>;
